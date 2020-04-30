71e892191cdcb189c1448ede1bc86c5a7498b8bc
kdean4227 <63613983+kdean4227@users.noreply.github.com>
2020-04-29 22:57:52 -0400

    Update SpinTP script
    Added oscillation and rotation on the TP

--- Great TP Run/Assets/ToiletPaper/Scripts/SpinTP.cs
+++ Great TP Run/Assets/ToiletPaper/Scripts/SpinTP.cs
@@ -1,10 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NETWORK_ENGINE;
 
-public class SpinTP : MonoBehaviour
+public class SpinTP : NetworkComponent
 {
-    // Start is called before the first frame update
+    //Original y position of the TP
+    public float originalY;
+    //floatation speed
+    public float floatationSpeed;
+    //Speed that the TP spins
+    public float rotationSpeed;
+    
+    public override void HandleMessage(string flag, string value)
+    {
+
+    }
+    
+    public override IEnumerator SlowUpdate()
+    {
+        originalY = transform.position.y;
+        rotationSpeed = 0.0f;
+        
+        while (true)
+        {
+            if(IsServer)
+            {
+                //oscillation
+                transform.position = new Vector3(transform.position.x, (originalY + Mathf.Sin(floatationSpeed * 0.0001f) * 0.05f), transform.position.z);
+                //rotation
+                transform.Rotate(0,rotationSpeed,0);
+                floatationSpeed++;
+                rotationSpeed++;
+            }
+
+            yield return new WaitForSeconds(MyCore.MasterTimer);
+        }
+    }
+    
     void Start()
     {
         
